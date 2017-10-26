c51b3fa87d2912e173db5e6234d05d43a9587125
Mark van der Velden <mark@dynom.nl>
2017-10-26 15:58:10 +0200

    Fixing request log timing

--- handlers/requestlogger.go
+++ handlers/requestlogger.go
@@ -12,11 +12,12 @@ import (
 func NewRequestLogger(l log.Logger) func(h http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			start := time.Now()
-			l.Log("request", "start", "path", r.URL.Path, "remote_addr", r.RemoteAddr)
-			defer l.Log("request", "end", "duration", time.Since(start))
 
+			l.Log("request", "start", "path", r.URL.Path, "remote_addr", r.RemoteAddr)
+
+			start := time.Now()
 			h.ServeHTTP(w, r)
+			l.Log("request", "end", "duration", time.Since(start))
 		})
 	}
 }
